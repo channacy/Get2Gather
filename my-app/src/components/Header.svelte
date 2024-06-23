3f8bb5808b321d5f7691a72a8d8237f1814d660e
channacy <68759286+channacy@users.noreply.github.com>
2024-06-23 09:58:27 -0400

    add basic header component

---
+++ my-app/src/components/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { Button } from "bits-ui";
+</script>
+
+<nav>
+    <h1>Get2Gether</h1>
+
+    <h1>Let's Meet</h1>
+    <h1>About</h1>
+
+    <Button.Root class="inline-flex h-12 items-center justify-center rounded-input bg-dark
+        px-[21px] text-[15px] font-semibold text-background shadow-mini
+        hover:bg-dark/95 active:scale-98 active:transition-all">
+    Login
+</Button.Root>
+</nav>
