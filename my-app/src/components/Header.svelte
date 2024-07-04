31aff90277f45e848e3afec59c94502357f8cd5a
Channacy <68759286+channacy@users.noreply.github.com>
2024-07-03 22:51:03 -0400

    add navigation bar component

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -2,14 +2,38 @@
     import { Button } from "bits-ui";
 </script>
 
-<nav>
-    <h1>Get2Gether</h1>
-
-    <h1>Let's Meet</h1>
-    <h1>About</h1>
-
-    <Button.Root class="inline-flex h-12 items-center justify-center rounded-input bg-dark
-        px-[21px] text-[15px] font-semibold text-background shadow-mini
-        hover:bg-dark/95 active:scale-98 active:transition-all bg-indigo-500">Login
-    </Button.Root>
+<nav class="navbar">
+    <h1 class="text-2xl	text-gray-900 decoration-stone-900 decoration-2">Get2Gether</h1>
+    <div class="sub-navbar">
+        <a href="/" class="text-lg">Let's Meet</a>
+        <a href="/about" class="text-lg">About</a>
+        <Button.Root class="inline-flex h-12 items-center justify-center rounded-input bg-dark
+            px-[21px] text-[15px] font-semibold text-background shadow-mini
+            hover:bg-dark/95 active:scale-98 active:transition-all bg-slate-900	text-gray-50">Login
+        </Button.Root>
+    </div>
 </nav>
+
+<style>
+    nav {
+        padding: 0.5rem;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+
+    .navbar{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .navbar a:hover{
+        color:black;
+        text-decoration: underline;
+    }
+    
+    .sub-navbar {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+    }
+</style>
