223e5ab0d28a81c774a5c98d417411f3e075a764
Channacy <68759286+channacy@users.noreply.github.com>
2024-07-04 00:11:10 -0400

    update navigation bar header component and add calendar component

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -9,7 +9,7 @@
         <a href="/about" class="text-lg">About</a>
         <Button.Root class="inline-flex h-12 items-center justify-center rounded-input bg-dark
             px-[21px] text-[15px] font-semibold text-background shadow-mini
-            hover:bg-dark/95 active:scale-98 active:transition-all bg-slate-900	text-gray-50">Login
+            hover:bg-dark/95 active:scale-98 active:transition-all bg-slate-900	text-gray-50"><a href="/login">Login</a>
         </Button.Root>
     </div>
 </nav>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,65 @@
+<script lang="ts">
+  import {Select} from "bits-ui";
+  import SingleCalendar from "../components/SingleCalendar.svelte";
+
+  const meetingTypes = [
+    { value: "Weekly", label: "Weekly" },
+    { value: "Single", label: "Single" },
+  ];
+</script>
+ 
+<div class="body place-content-center">
+  <div class="time-section">
+    <h1>1. Select Meeting Type and Day</h1>
+    <div class="meeting-type-select">
+      <Select.Root items={meetingTypes}>
+        <Select.Trigger
+            class="inline-flex h-input w-[296px] items-center rounded-9px border border-border-input bg-background px-[11px] text-sm transition-colors placeholder:text-foreground-alt/50  focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-offset-2 focus:ring-offset-background"
+            aria-label="Select a meeting type">
+          <Select.Value placeholder="Select a meeting type"/>
+        </Select.Trigger>
+
+        <Select.Content
+          class="w-full rounded-xl border border-muted bg-background px-1 py-3 shadow-popover outline-none"
+          sideOffset={8}
+        >
+        {#each meetingTypes as meetingType}
+          <Select.Item
+            class="flex h-10 w-full select-none items-center rounded-button py-3 pl-5 pr-1.5 text-sm outline-none transition-all duration-75 data-[highlighted]:bg-muted"
+            value={meetingType.value}
+            label={meetingType.label}
+          >
+            {meetingType.label}
+          </Select.Item>
+        {/each}
+      </Select.Content>
+      <Select.Input name="selectedMeetingType" />
+      </Select.Root>
+    </div>
+    <div class="calendar">
+      <SingleCalendar />
+    </div>
+  </div>
+  <div class="info-section">
+    <h1>2. Add Meeting Information</h1>
+  </div>
+</div>
+
+
 <style lang="postcss">
   :global(html) {
     background-color: theme(colors.gray.100);
+  }
+
+  .body {
+    display: flex;
+    flex-direction: row;
+
+  }
+
+  .time-section, .info-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
